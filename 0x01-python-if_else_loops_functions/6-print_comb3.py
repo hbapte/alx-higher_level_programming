63556901f4dd2af8d0f63e10ab733ae086afba23
hbapte <ijbapte@gmail.com>
2022-08-23 23:19:15 +0000

    tasks

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-# We use i for the first digit and j for the second
-for i in range(0, 9):
-    for j in range(i + 1, 10):
-        if i is not 8 or j is not 9:
-            print("{:d}{:d}".format(i, j), end=", ")
+for digit1 in range(0, 10):
+    for digit2 in range(digit1 + 1, 10):
+        if digit1 == 8 and digit2 == 9:
+            print("{}{}".format(digit1, digit2))
         else:
-            print("{:d}{:d}".format(i, j))
+            print("{}{}".format(digit1, digit2), end=", ")
