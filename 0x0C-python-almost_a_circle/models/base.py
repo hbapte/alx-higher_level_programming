7748dc03a50c87f16a367b89a9ce6bfc5169a46f
hbapte <ijbapte@gmail.com>
2022-10-04 00:01:28 +0000

    test files

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""The Base Class"""
-
-
+"""base
+"""
 import json
-import os
+import csv
 
 
 class Base:
-    """The Base Class"""
+    """Base of the other shapes
+    """
     __nb_objects = 0
 
     def __init__(self, id=None):
-        """Class initialization"""
-
         if id is not None:
             self.id = id
         else:
@@ -21,49 +19,89 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
-        """Returns the JSON string representation of list_dictionaries"""
-        if list_dictionaries is None or list_dictionaries == {}:
-            return []
+        """Returns JSON string representation
+        """
+        if list_dictionaries is None or len(list_dictionaries) == 0:
+            return "[]"
         else:
             return json.dumps(list_dictionaries)
 
-    @classmethod
-    def save_to_file(cls, list_objs):
-        """Writes the JSON string representation of list_objs to a file"""
-        filename = cls.__name__ + ".json"
-        content = []
-        if list_objs is not None:
-            for item in list_objs:
-                content.append(item.to_dictionary())
-        with open(filename, 'w', encoding="utf-8") as f:
-            return f.write(cls.to_json_string(content))
-
     @staticmethod
     def from_json_string(json_string):
-        """Returns the list of the JSON string representation json_string"""
-        if json_string is None:
+        """Returns JSON strings in list
+        """
+
+        if type(json_string) != str or len(json_string) == 0:
             return []
-        else:
-            return json.loads(json_string)
+        return json.loads(json_string)
 
     @classmethod
     def create(cls, **dictionary):
-        """Returns an instance with all attributes already set"""
-        if dictionary != {}:
-            if cls.__name__ == 'Rectangle':
-                new_instance = cls(1, 1)
-            if cls.__name__ == 'Square':
-                new_instance = cls(1)
-        new_instance.update(**dictionary)
-        return new_instance
+        """Returns an instance with all attrs already set
+        """
+
+        if cls.__name__ == "Rectangle":
+            temp = cls(1, 1)
+        if cls.__name__ == "Square":
+            temp = cls(1)
+        # update temp with obj func update()
+        temp.update(**dictionary)
+        return temp
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Writes to file with JSON string
+        """
+
+        with open(cls.__name__ + ".json", mode="w") as write_file:
+            if list_objs is None:
+                write_file.write("[]")
+            else:
+                # Using to_json_string(), and to_dictionary() to format
+                write_file.write(cls.to_json_string(
+                                 [item.to_dictionary() for item in list_objs]))
 
     @classmethod
     def load_from_file(cls):
-        """Returns a list of instances"""
-        filename = cls.__name__ + ".json"
-        if not os.path.isfile(filename):
-            return []
-        with open(filename, 'r', encoding="utf-8") as f:
-            content = f.read()
-            formattedContent = (cls.from_json_string(content))
-            return [cls.create(**item) for item in formattedContent]
+        """Returns a list of instances
+        """
+
+        res = []
+        with open(cls.__name__ + ".json", mode="r") as read_file:
+            text = read_file.read()
+        # Converting str to list
+        text = cls.from_json_string(text)
+        for item in text:
+            # Formatting dicts into str format
+            if type(item) == dict:
+                res.append(cls.create(**item))
+            else:
+                res.append(item)
+        return res
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Saves to csv file
+        """
+
+        res = [item.to_dictionary() for item in list_objs]
+        with open(cls.__name__ + ".csv", mode="w") as save_file:
+            write_to = csv.DictWriter(save_file, res[0].keys())
+            write_to.writeheader()
+            write_to.writerows(res)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Loads from csv file
+        """
+
+        res = []
+        res_dict = {}
+        with open(cls.__name__ + ".csv", mode="r") as read_file:
+            read_from = csv.DictReader(read_file)
+            for item in read_from:
+                for k, v in dict(item).items():
+                    res_dict[k] = int(v)
+                # formatting with create()
+                res.append(cls.create(**res_dict))
+        return res
--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,45 +1,53 @@
 #!/usr/bin/python3
-"""The Square Class"""
-
-
+"""square
+"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Square class which inherits from the rectangle class"""
+    """Inherits from Rectangle
+    """
+
     def __init__(self, size, x=0, y=0, id=None):
-        """initialization"""
-        super().__init__(size, size, x, y, id)
+        super().__init__(width=size, height=size, x=x, y=y, id=id)
 
     @property
     def size(self):
-        """returns the size property which
-        is the same as the width and height
-        """
         return self.width
 
     @size.setter
     def size(self, value):
-        """sets the size property"""
-        self.width = self.height = value
-
-    def __str__(self):
-        """Public str method"""
-        return "[Square] ({:d}) {:d}/{:d} - {:d}"\
-            .format(self.id, self.x, self.y, self.width)
-
-    def update(self, *args, **kwargs):
-        """update square attributes
+        """size needs to be an int
         """
 
-        expectedArgs = (self.id, self.size, self.x, self.y)
-        if args != ():
-            self.id, self.size, self.x, self.y = \
-                args + expectedArgs[len(args):len(expectedArgs)]
-        elif kwargs:
-            for (name, value) in kwargs.items():
-                setattr(self, name, value)
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """Returns formatted information display
+        """
+
+        return "[{}] ({}) {}/{} - {}".format(self.__class__.__name__,
+                                             self.id, self.x, self.y,
+                                             self.width)
+
+    def update(self, *args, **kwargs):
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                setattr(self, k, v)
+        elif len(args) != 0:
+            try:
+                self.id = args[0]
+                self.size = args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except IndexError:
+                pass
+        else:
+            print()
 
     def to_dictionary(self):
-        """Returns a dictionary representation of a rectangle"""
-        return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+        """Returns a dict representation
+        """
+
+        return {'id': self.id, 'x': self.x, 'size': self.width, 'y': self.y}
