823797876e5ff7f54094825680a98cd7cc74c192
hbapte <ijbapte@gmail.com>
2022-11-29 21:19:35 +0000

    js object,scopes and closures

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,17 +1,13 @@
 #!/usr/bin/node
-const dict = require('./101-data').dict;
-
-const totalist = Object.entries(dict);
-const vals = Object.values(dict);
-const valsUniq = [...new Set(vals)];
+const dict = require('./101-data.js').dict;
 const newDict = {};
-for (const j in valsUniq) {
-  const list = [];
-  for (const k in totalist) {
-    if (totalist[k][1] === valsUniq[j]) {
-      list.unshift(totalist[k][0]);
+for (const value of Object.values(dict)) {
+  const newList = [];
+  newDict[value] = newList;
+  for (const k in dict) {
+    if (dict[k] === Number(value)) {
+      newList.push(k);
     }
   }
-  newDict[valsUniq[j]] = list;
 }
 console.log(newDict);
